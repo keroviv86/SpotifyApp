813d35662fcdeddb7859026ae805f56910b2b3f7
Vivian Yim <keroviv86@gmail.com>
2022-04-14 21:14:41 -0500

    finished setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,37 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import '../styles/globals.css'
+import "reset-css";
+// allows all browser to have the same css
 
-const MyApp=({ Component, pageProps }) => {
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5f5f5',
+      200: '#EEEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121'
+    },
+  },
+  componetns: {
+    Button: {
+      varients: {
+        link: {
+          ':focus': {
+            outline: "none",
+            boxShadow: "none",
+          }
+        }
+      }
+      // varients is the type of button 
+    }
+  }
+})
+
+const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
